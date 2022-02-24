a14528c3ca39860d0b8ba2eed6939efb9e47fa97
vjcitn <stvjc@channing.harvard.edu>
2022-02-24 09:45:06 -0700

    now has jupyter

--- inst/map_app/ui.R
+++ inst/map_app/ui.R
@@ -10,14 +10,17 @@ providers_of_interest = c("CartoDB.PositronNoLabels",
 ui = fluidPage(
  sidebarLayout(
   sidebarPanel(
-   helpText("Basic overview of mapping with R"),
+   helpText(h2("Basic overview of mapping with R")),
+   helpText("With decent internet, you can just pan and zoom with controls on the map."),
    selectInput("provider", "type", choices=c("CartoDB", "Stamen"), 
      selected="Stamen"),
+   helpText("Coordinates to start:"),
    numericInput("lng", "longitude", min=-360, max=360, value=-71.0589),
    numericInput("lat", "latitude", min=-360, max=360, value=42.3601),
    numericInput("zoom", "zoom", min=1, max=18, value=12),
    radioButtons("labs", "labels", choices=c("on", "off")),
-   actionButton("stopBtn", "stop app")
+   actionButton("stopBtn", "stop app"),
+   width=3
   ),
   mainPanel(
    tabsetPanel(
