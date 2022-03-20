6d195524e46df63fb7ec52b57f3f7c18ac73814b
vjcitn <stvjc@channing.harvard.edu>
2022-03-20 13:25:23 +0100

    table.only in cancer_map_usa

--- R/map_app.R
+++ R/map_app.R
@@ -13,9 +13,11 @@ map_app = function() {
 #' simple function for site-specific map
 #' @param site character(1) defaults to "Stomach", must lie in value of woncan_types()
 #' @param scaling numeric(1) scales the radius of circles, defaults to 1
+#' @param table.only logical(1) defaults to FALSE; if TRUE just produces CDC WONDER data filtered to selected site
 #' @export
-cancer_map_usa = function( site = "Stomach", scaling = 1 ) {
+cancer_map_usa = function( site = "Stomach", scaling = 1, table.only=FALSE ) {
     dat = filter_woncan(site=site)
+    if (table.only) return(dat)
     suppressWarnings({
       m = us_map() |> addCircleMarkers(lat=dat$lat, lng=dat$lng, radius=scaling * dat$aarate,
           # popup=htmltools::htmlEscape(paste( site, ": ", dat$aarate, sep="")))
